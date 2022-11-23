8c53118660e644cc534f2a42975093af765c899e
Gustavo <gustavo.henrique.mm08@gmail.com>
2022-11-23 16:59:09 -0300

    commit7

--- src/scripts/script_pagcadastro.js
+++ src/scripts/script_pagcadastro.js
@@ -5,7 +5,7 @@ function trocarInputs(numeroTela){
     switch(numeroTela){
         case 1: 
 
-            if(!(inputs[0].value) || !(inputs[1].value) || !(inputs[2].value) || !(inputs[3].value) || inputs[2].value != inputs[3].value){
+            if(!(inputs[0].value) && !(inputs[1].value) && !(inputs[2].value) && !(inputs[3].value) && inputs[2].value != inputs[3].value){
                 msgErro = "Preencha todos os campos";
                 if(!(inputs[0].value)){
                     document.querySelector('.texto1').innerHTML = msgErro
@@ -22,7 +22,7 @@ function trocarInputs(numeroTela){
                     document.querySelector('.texto2').innerHTML = ''
                 }
                 
-                if(!(inputs[2].value) || !(inputs[3].value) || inputs[2].value != inputs[3].value){
+                if(!(inputs[2].value) && !(inputs[3].value) && inputs[2].value != inputs[3].value){
                     if(!(inputs[2].value)){
                         document.querySelector('.senha-error').innerHTML = msgErro
                         break
@@ -40,10 +40,12 @@ function trocarInputs(numeroTela){
                     if(inputs[2].value != inputs[3].value){
                         document.querySelector('.senha-error').innerHTML = "As senhas são diferentes"
                         document.querySelector('.senha-error2').innerHTML = "As senhas são diferentes"
+                        break
                     }
                     else{
                         document.querySelector('.senha-error').innerHTML = ""
                         document.querySelector('.senha-error2').innerHTML = ""
+                        break
                     }
                 }   
                 
@@ -51,8 +53,9 @@ function trocarInputs(numeroTela){
             else{
                 img = document.querySelector('.ondinha1')
                 img.src = './src/imgs/voltar.png'
-                img.onclick = trocarInputs(2)
-                document.querySelector('.avancar').style.display = "none"
+                img.setAttribute('onclick', 'trocarInputs(2);');
+                document.querySelector('.avancar').src = './src/imgs/finalizar.png';
+                document.querySelector('.cadastro--submit').setAttribute('for','finalizar');
 
                 //img.classList = "teste"
                 blocos = document.querySelectorAll('.cadastro--input')
@@ -60,8 +63,28 @@ function trocarInputs(numeroTela){
                     blocos[i].style.display='none'                            
                 }
                 document.querySelector('.cadastro--input__ativada').style.display = "flex"
-                
+                document.querySelector('.ondinha1').classList.toggle('voltar');
+
+                break
             }
+
+        case 2: 
+            img = document.querySelector('.ondinha1');
+            img.src = './src/imgs/img-cad1.png';
+            img.setAttribute('onclick', '');
+            img.classList.toggle('voltar');
+            document.querySelector('.avancar').src = './src/imgs/seta.png'
+            document.querySelector('.cadastro--submit').setAttribute('for','');
+       
+            //img.classList = "teste"
+            blocos = document.querySelectorAll('.cadastro--input');
+            for(i=0;i<4;i++){
+                blocos[i].style.display='flex';
+            }
+            document.querySelector('.cadastro--input__ativada').style.display = "none";
+    
+
+
         break
     }
 }
