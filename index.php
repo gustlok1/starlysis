2bbecf791b11a1ad36bf92d6d03e496da96b7207
Gustavo <gustavo.henrique.mm08@gmail.com>
2022-11-25 23:12:22 -0300

    commit13

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="src/css/style_pagprincipal.css">
     <link rel="stylesheet" href="src/css/formatacao.css">
+    <link rel="stylesheet" href="src/css/footer.css">
     <title>Página Principal - STARLYSIS</title>
 </head>
 <body>
@@ -17,7 +18,7 @@
 
     <main class="main">        
         <section class="main--textos center">
-            <h1 class="main--titulo"><span class="c-laranja bold">Analisamos <br> </span> os seus dados e <span class="c-laranja bold"><br>alavancamos<br></span> o seu negócio</h1>
+            <h1 class="main--titulo" id="analise"><span class="c-laranja bold">Analisamos <br> </span> os seus dados e <span class="c-laranja bold"><br>alavancamos<br></span> o seu negócio</h1>
             <p class="main--texto">A análise de dados é um processo de inspeção, limpeza, transformação e modelagem de dados com o objetivo 
                 de descobrir informações úteis, informar conclusões e apoiar a tomada de decisões. A análise de dados tem múltiplas facetas 
                 e abordagens, abrangendo diversas técnicas sob uma variedade de nomes, e é usada em diferentes domínios dos negócios, 
@@ -32,6 +33,38 @@
         <img src="" alt="">
         <section></section>
     </main>
+
+    <footer class="footer">
+        <ul class="footer--ul">
+            <li class="footer--itens img"><a href="./index.php"><img src="./src/imgs/logo.png" alt="logo"></a></li>
+            <li class="footer--itens index"><ul class="footer--itens--index">
+                <li class="footer--itens--index--titulo"><a href="./index.php">Página Principal</a></li>
+                <li><a href="./index.php#analise">Analise</a></li>
+                <li><a href="./index.php#gerador">Gerador</a></li>
+                <li><a href="./index.php#analise">Analise</a></li>
+            </ul></li>
+
+            <li class="footer--itens sobre"><ul class="footer--itens--sobre">
+                <li class="footer--itens--sobre--titulo"><a href="./sobre_starlysis.php">Sobre nós</a></li>
+                <li><a href="./sobre_starlysis.php#analise">Analise</a></li>
+                <li><a href="./sobre_starlysis.php#quem-somos">Quem somos?</a></li>
+                <li><a href="./sobre_starlysis.php#nossa-historia">Nossa história</a></li>
+            </ul>
+            </li>
+
+            <li class="footer--itens suporte"><ul class="footer--itens--suporte">
+                <li class="footer--itens--suporte--titulo"><a href="./suporte_starlysis.php">Suporte</a></li>
+                <li><a href="./suporte_starlysis.php#">Mandar email</a></li>
+            </ul>
+            </li>
+            <li class="footer--itens redes"><ul class="footer--itens--redes">
+                <li><a href="https://twitter.com"><img src="./src/imgs/devicon-master/devicon-master/icons/twitter/twitter-original.html" alt="twitter"></a></li>
+                <li><a href="./sobre_starlysis.php#quem-somos">Quem somos?</a></li>
+                <li><a href="./sobre_starlysis.php#nossa-historia">Nossa história</a></li>
+            </ul>
+        </li>
+        </ul>
+    </footer>
 </body>
 
 <script src="./src/scripts/script_pagprincipal.js"></script>
--- sobre_starlysis.php
+++ sobre_starlysis.php
@@ -17,16 +17,24 @@
     ?>
     <main class="main1">
         <section class="main1--textos">
-            <h1>ANÁLISE DE <br> DADOS</h1>
-            <p>A análise de dados.</p>
+            <h1 id="analise">Análise de <br>dados</h1>
+            <p>A análise de dados é uma forma de observar os resultados para poder gerar informações e por conseguinte, conhecimento. O objetivo dessa empresa é expandir os horizontes desse processo tão importante no meio comecial mundial atual.</p>
         </section>
         <img src="./src/imgs/graphics.png" alt="gráfico" class="main1--grafico">
     </main>
 
     <main class="main1 m2">
-    <section class="main1--textos">
-            <h1>Quem somos nós?</h1>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+    <img src="./src/imgs/equipe-conversando1.png" alt="equipe conversando" class="main2--equipe">
+    <section class="main2--textos">
+            <h1 id="quem-somos">Quem somos nós?</h1>
+            <p>Nós somos uma empresa que visa o processo de compreensão dos dados, facilitando a mesma através da automatização de um processo pode acabar sendo muito complicado para pessoas que não tem conhecimento em programação, possibilitando uma agilidade maior e reduzindo o tempo gasto no processo, que poderá ser utilizado para tomada de decisões mais inteligentes.</p>
+        </section>
+    </main>
+
+    <main class="main1 m3">
+    <section class="main2--textos">
+            <h1 id="nossa-historia">Nossa história</h1>
+            <p>Nós somos uma empresa que visa o processo de compreensão dos dados, facilitando a mesma através da automatização de um processo pode acabar sendo muito complicado para pessoas que não tem conhecimento em programação, possibilitando uma agilidade maior e reduzindo o tempo gasto no processo, que poderá ser utilizado para tomada de decisões mais inteligentes.</p>
         </section>
     </main>
 </body>
